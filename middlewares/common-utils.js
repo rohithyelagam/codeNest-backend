ecd1a000427512641201516323a7412addd43ac9
Rohith.Yelagam <Rohith.Yelagam@ril.com>
2024-10-05 12:53:55 +0530

    search algo fix

--- middlewares/common-utils.js
+++ middlewares/common-utils.js
@@ -21,14 +21,14 @@ const getScore = (X,Y,m,n)=>{
     for (var i = 0; i <= m; i++) {
         for (var j = 0; j <= n; j++) {
 
-            if (i == 0 || j == 0)
+            if (i == 0 || j == 0){
                 L[i][j] = 0;
-
-            else if (X[i - 1] == Y[j - 1])
-                L[i][j] = L[i - 1][j - 1] + 1;
-
-            else
-                L[i][j] = Math.max(L[i - 1][j], L[i][j - 1]);
+                continue;
+            }
+            L[i][j] = Math.max(L[i - 1][j], L[i][j - 1]);
+            if(X[i - 1] == Y[j - 1]){
+                L[i][j] = Math.max(L[i - 1][j - 1] + 1,L[i][j]);
+            }
         }
     }
 
